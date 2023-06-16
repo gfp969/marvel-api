64ca9b3bad0d61781563d4cb7a1199a86b7d2431
Felipe <felipe9.9.9@outlook.com>
2023-06-16 10:50:15 -0500

    se le agregaron estilos a las cartas de personajes y a la pagina en general

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import MarvelApp from "./components/MarvelApp";
+import "./style.css"
 
 function App() {
   return (
-    <div className="App">
+    <div className="header">
+      <img src="../img/marvel.png" width="300px" alt=""/>
       <h1>MARVEL</h1>
       <MarvelApp />
     </div>
--- src/components/MarvelApp.js
+++ src/components/MarvelApp.js
@@ -1,21 +1,32 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function MarvelApp() {
+    const [personajes, setPersonajes] = useState([]);
     const hash = process.env.REACT_APP_HASH;
     const key = process.env.REACT_APP_PUBLIC_KEY;
-    const url = `${process.env.REACT_APP_URL}&apikey=${key}&hash=${hash}`; 
+    const url = `${process.env.REACT_APP_URL}&apikey=${key}&hash=${hash}`;
 
     useEffect(() => {
         axios.get(url)
             .then(resp => {
+                setPersonajes(resp.data.data.results);
                 console.log(resp.data);
             })
+            .catch(error => { console.log(error) })
     }, [])
+    console.log(personajes);
 
     return (
-        <div>
-
+        <div className="card-group">
+            {personajes.map(per => (
+                <div class="card" key={per.id}>
+                    <img src={`${per.thumbnail.path}.${per.thumbnail.extension}`} class="card-img-top" width="200px" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{per.name}</h5>
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
